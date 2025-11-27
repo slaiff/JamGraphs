ec497681a71d7bbfe5264eb14320cea5cdf1a261
Pavlo Kinyshchuk <pavlokinisuk@gmail.com>
2025-11-27 10:00:38 -0800

    account data

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 var session = require('express-session')
 var querystring = require('querystring')
 var post = require('post')
-var account = require('account')
+var account = require('account') /*
+                                    methods {
+                                        userExists(userId)
+                                        newUser(userId, refreshToken)
+                                        getUser(userId)
+                                        updateRefreshToken(userId, refreshToken)
+                                    }
+                                */
 
 app.set("view engine", 'ejs')
 
@@ -13,12 +20,12 @@ app.use(express.static('public'))
 
 
 //spotify login route
-var client_id = '0ef3e0750fd94db79804a645ca247f1b';
-var redirect_uri = 'http://127.0.0.1:8000/callback';
-var client_secret = 'fd6e18de464142c28df7194022d82907';
+var client_id = '0ef3e0750fd94db79804a645ca247f1b'
+var redirect_uri = 'http://127.0.0.1:8000/callback'
+var client_secret = 'fd6e18de464142c28df7194022d82907'
 
 function generateRandomString(length) {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
   let result = '';
   for (let i = 0; i < length; i++) {
     result += chars[Math.floor(Math.random() * chars.length)];
@@ -29,6 +36,7 @@ function generateRandomString(length) {
 app.use(session({
   secret: 'random-secret',
   resave: false,
+  saveUninitialized: true //not sure what this is for but cant run wout it
 }));
 
 app.get('/login', function(req, res) {
@@ -46,14 +54,14 @@ app.get('/login', function(req, res) {
 });
 
 app.get('/callback', async function(req, res) {
-  var code = req.query.code || null;
-  var state = req.query.state || null;
+  var code = req.query.code || null
+  var state = req.query.state || null
 
   if (state === null || state !== req.session.state) {
-    return res.status(400).send('State mismatch');
+    return res.status(400).send('State mismatch')
   }
 
-  req.session.state = null;
+  req.session.state = null
 
   try {
     const tokenRes = await fetch('https://accounts.spotify.com/api/token', {
@@ -67,10 +75,10 @@ app.get('/callback', async function(req, res) {
         redirect_uri: redirect_uri,
         grant_type: 'authorization_code'
       })
-    });
+    })
 
-    const tokenData = await tokenRes.json();
-    console.log(tokenData);
+    const tokenData = await tokenRes.json()
+    console.log(tokenData)
 
     req.session.access_token = tokenData.access_token;
     req.session.refresh_token = tokenData.refresh_token;
@@ -79,12 +87,20 @@ app.get('/callback', async function(req, res) {
       headers: { 'Authorization': 'Bearer ' + tokenData.access_token }
     });
 
-    const profileData = await profileRes.json();
-    req.session.userId = profileData.id;
+    const profileData = await profileRes.json()
+    req.session.userId = profileData.id
 
-    res.redirect('/account'); //since userId exists, should render account
+
+    if (account.userExists(req.session.userId)) {
+        account.updateRefreshToken(req.session.userId, req.session.refresh_token)
+    } else {
+        account.newUser(req.session.userId, req.session.refresh_token)
+    }
+    account.newUser(req.session.userId, req.session.refresh_token)
+
+    res.redirect('/account') //since userId exists, should render account
   } catch (err) {
-    console.error(err);
+    console.error(err)
     res.status(500).send('Error during token exchange or fetching profile');
   }
 });
@@ -110,6 +126,10 @@ app.get("/account", function (req, res) {
 
 app.get("/feed", function (req, res) {
     res.render("feed")
+})
+
+app.get("/about", function (req, res) {
+    res.render("about")
 })
 
 app.listen(8000, function () {
